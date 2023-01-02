d359d2b544cf1693321905b4ee74f5cb6a0f2225
Anton Adamsson <119974909+lilleanton@users.noreply.github.com>
2023-01-02 15:56:18 +0100

    Added documentation

--- src/draw.py
+++ src/draw.py
@@ -1,24 +1,29 @@
+import contextlib
+with contextlib.redirect_stdout(None):
+    import pygame
+from pickle import load
 import random
-
-import pygame
 import math
-import pickle
-from main import*
 
+
+# drawMode() used for testing a network with user-created data. Requires Pygame.
 def drawMode(path):
+    # Create Pygame context
     pygame.init()
     screen = pygame.display.set_mode([500, 500])
+    pygame.display.set_caption(f'drawMode("{path}")')
     font = pygame.font.Font('freesansbold.ttf', 32)
 
+    # Drawing grid
     grid = [[float(0) for j in range(28)] for i in range(28)]
 
-    network = pickle.load(open(path, 'rb'))
+    # Import a network
+    network = load(open(path, 'rb'))
 
-    timing = 0
     running = True
     while running:
-        timing += 1
 
+        # User events handling, wiping the grid and exiting the program
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -27,19 +32,12 @@ def drawMode(path):
                 if event.key == pygame.K_SPACE:
                     grid = [[float(0) for j in range(28)] for i in range(28)]
 
-        screen.fill((0, 0, 0))
-        for i in range(28):
-            for j in range(28):
-                color = (int(grid[i][j] * 255), int(grid[i][j] * 255), int(grid[i][j] * 255))
-                pygame.draw.rect(screen, color,
-                                 pygame.Rect(j * 500 / 28, i * 500 / 28, 500 / 28 + 1, 500 / 28 + 1))
-
+        # Get mouse input
         gridx = math.floor(pygame.mouse.get_pos()[0] / (500 / 28))
         gridy = math.floor(pygame.mouse.get_pos()[1] / (500 / 28))
 
+        # Simple drawing function, approximates the MNIST brush
         if pygame.mouse.get_pressed()[0]:
-
-            i = 0
             grid[gridy][gridx] = 1.0
 
             for i in [-1, 0, 1]:
@@ -50,14 +48,25 @@ def drawMode(path):
                     if grid[gridy + i][gridx + j] == 0:
                         grid[gridy + i][gridx + j] = random.uniform(0.4, 0.8)
 
+        # Drawing the input
+        screen.fill((0, 0, 0))
+        for i in range(28):
+            for j in range(28):
+                color = (int(grid[i][j] * 255), int(grid[i][j] * 255), int(grid[i][j] * 255))
+                pygame.draw.rect(screen, color,
+                                 pygame.Rect(j * 500 / 28, i * 500 / 28, 500 / 28 + 1, 500 / 28 + 1))
+
+        # Get network guess
         network.inputData([grid[j][i] for j in range(28) for i in range(28)])
         network.propagate()
         guess = str([index for index, item in enumerate(network.output()) if item == max(network.output())][0])
+
+        # Display guess
         text = font.render(guess, True, (255, 255, 255))
         textRect = text.get_rect()
         textRect.center = (20, 20)
         screen.blit(text, textRect)
 
-        pygame.display.flip()
+        pygame.display.flip()  # Render
 
     pygame.quit()
