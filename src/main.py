26e0485e44af79381447366920a64a510a41b0a5
Anton Adamsson <119974909+lilleanton@users.noreply.github.com>
2023-01-02 15:14:43 +0100

    Migrating to mnist.py and network.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import draw
+from network import Network, TrainingPack
+import mnist
+
+if __name__ == '__main__':
+    # Examples
+    # draw.drawMode('net.pkl')
+    # mnist.testnet('net.pkl')
+    # mnist.trainNet('net.pkl')
+    pass
